97fb4a8684e83f47365e8db3ad514f92197dbd8b
jeferson santos <jefersondeoliveira062@gmail.com>
2024-10-20 20:32:35 -0300

    fix: Corrigi campo creci para aceitar valores nulos

--- usuarios/models.py
+++ usuarios/models.py
@@ -6,7 +6,7 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=255, null=False, blank=False)
     login = models.CharField(max_length=100, unique=True, null=False, blank=False)
     senha = models.CharField(max_length=100, null=False, blank=False)
-    creci = models.CharField(max_length=100, unique=True)
+    creci = models.CharField(max_length=100, unique=True, null=True)
     cargo = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
