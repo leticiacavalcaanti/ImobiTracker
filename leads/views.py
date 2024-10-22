c6a8e2ddb1b4aad0227bbf199f9b416ca5d0d8b4
jeferson santos <jefersondeoliveira062@gmail.com>
2024-10-22 20:45:46 -0300

    feat: Adiciona metodos de transacao nas views leads e usuarios

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,22 @@
+from django.db import transaction
 from leads.models import Lead
 from leads.serializers import LeadSerializers
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 class LeadsViewSet(viewsets.ModelViewSet):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializers
+
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        email = request.data.get('email')
+
+        # Verifica se o email já existe, se existe retorna erro 409
+        if Lead.objects.filter(email=email).exists():
+            return Response({"detail": "Email já existe."}, status=status.HTTP_409_CONFLICT)
+
+        #cria um novo registro
+        return super().create(request, *args, **kwargs)
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,21 @@
+from django.db import transaction
 from rest_framework import viewsets
 from usuarios.models import Usuario
 from usuarios.serializers import UsuarioSerializers
+from rest_framework.response import Response
+from rest_framework import status
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializers
 
- 
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        login = request.data.get('login')
+
+        # Verifica se o login já existe, se existe retorna erro 409
+        if Usuario.objects.filter(login=login).exists():
+            return Response({"detail": "login já existe."}, status=status.HTTP_409_CONFLICT)
+
+        #cria um novo registro
+        return super().create(request, *args, **kwargs)
